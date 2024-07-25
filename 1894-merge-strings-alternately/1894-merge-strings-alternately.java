0349560d1c0ccd6ae3246687288acce50adf9c53
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2024-07-25 21:19:24 +0530

    Time: 1 ms (86.68%), Space: 41.6 MB (54.63%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int n = Math.max(word1.length(), word2.length());
+        StringBuilder str = new StringBuilder(); 
+
+        for (int i = 0; i < n; i++) {
+            if (i < word1.length()) {
+                str.append(word1.charAt(i));           
+            }
+            if (i < word2.length()) {
+                str.append(word2.charAt(i));
+            }
+        }
+
+        return str.toString();
+        
+    }
+}
