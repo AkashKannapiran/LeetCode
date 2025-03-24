eb5b9eda2114d8cf406e434003b343a4b55b3051
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-03-24 16:59:48 +0530

    Time: 1 ms (94.46%), Space: 45.6 MB (16.71%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[nums.length * 2];
-        
+        int[] ans = new int[2 * nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[i];
-            ans[i + nums.length] = nums[i];
+            ans[i] = ans[i + nums.length] = nums[i];
         }
 
         return ans;
