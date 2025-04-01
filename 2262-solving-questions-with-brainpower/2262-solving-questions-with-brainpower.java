23c0861cd11258177582bbc6848a4a7d5306afbb
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-04-01 21:45:34 +0530

    Time: 5 ms (96.96%), Space: 108.2 MB (65.18%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int points = questions[i][0];
+            int brainPower = questions[i][1];
+            int next = i + brainPower + 1;
+            long take = points + (next < n ? dp[next] : 0);
+            long skip = dp[i + 1];
+            dp[i] = (take > skip) ? take : skip;
+        }
+
+        return dp[0];
+    }
+}
