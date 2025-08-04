9a730eefcdacac9ef4fd78c0ce64f1c2f4670350
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-08-04 17:23:31 +0530

    Time: 3 ms (100%), Space: 55 MB (76.68%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,29 +1,54 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        int start = 0;
-        int end = 0;
-        int n = fruits.length;
-        int maxLen = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        while (end < n) {
-            map.put(fruits[end], map.getOrDefault(fruits[end], 0) + 1);
-            
-            while (map.size() >= 3) {
-                map.put(fruits[start], map.get(fruits[start]) - 1);
-                
-                if (map.get(fruits[start]) == 0) {
-                    map.remove(fruits[start]);
+    static {
+        for (int i = 0; i < 300; i++) {
+            totalFruit(new int[0]);
+        }
+    }
+
+    public static int totalFruit(int[] fruits) {
+        int max = 0;
+        int type1 = -1;
+        int type2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        int lead = -1;
+        int leadIndex = -1;
+
+        for (int i = 0; i < fruits.length; i++) {
+            if (type1 == -1 || type1 == fruits[i]) {
+                type1 = fruits[i];
+                count1++;
+
+                if (lead != type1) {
+                    lead = type1;
+                    leadIndex = i;
+                }
+            } else if (type2 == -1 || type2 == fruits[i]) {
+                type2 = fruits[i];
+                count2++;
+
+                if (lead != type2) {
+                    lead = type2;
+                    leadIndex = i;
+                }
+            } else {
+                max = Math.max(max, count1 + count2);
+
+                if (lead == type1) {
+                    count1 = i - leadIndex;
+                    type2 = fruits[i];
+                    count2 = 1;
+                } else {
+                    count2 = i - leadIndex;
+                    type1 = fruits[i];
+                    count1 = 1;
                 }
 
-                start++;
+                lead = fruits[i];
+                leadIndex = i;
             }
-
-            int currLen = end - start + 1;
-            maxLen = Math.max(maxLen, currLen);
-            end++;
         }
-        
-        return maxLen;
+
+        return Math.max(max, count1 + count2);
     }
 }
