82c1e33c732b2816376e9d7fa2213d61487f1c27
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-08-07 00:49:28 +0530

    Time: 31 ms (100%), Space: 57.5 MB (91.89%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.java
@@ -1,89 +1,49 @@
 class Solution {
-
-    private int[] segTree = new int[400007];
-    private int[] baskets;
-
-    private void build(int p, int l, int r) {
-        if (l == r) {
-            segTree[p] = baskets[l];
-            return;
-        }
-
-        int mid = (l + r) >> 1;
-        build(p << 1, l, mid);
-        build((p << 1) | 1, mid + 1, r);
-        segTree[p] = Math.max(segTree[p << 1], segTree[(p << 1) | 1]);
-    }
-
-    private int query(int p, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) {
-            return Integer.MIN_VALUE;
-        }
-        
-        if (ql <= l && r <= qr) {
-            return segTree[p];
-        }
-        
-        int mid = (l + r) >> 1;
-        
-        return Math.max(
-            query(p << 1, l, mid, ql, qr),
-            query((p << 1) | 1, mid + 1, r, ql, qr)
-        );
-    }
-
-    private void update(int p, int l, int r, int pos, int val) {
-        if (l == r) {
-            segTree[p] = val;
-            return;
-        }
-        
-        int mid = (l + r) >> 1;
-        
-        if (pos <= mid) {
-            update(p << 1, l, mid, pos, val);
-        } else {
-            update((p << 1) | 1, mid + 1, r, pos, val);
-        }
-        
-        segTree[p] = Math.max(segTree[p << 1], segTree[(p << 1) | 1]);
-    }
-
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        this.baskets = baskets;
-        int m = baskets.length;
+        int n = baskets.length;
+        int N = 1;
+        
+        while(N <= n) {
+            N <<= 1;
+        }
+
+        int[] segTree = new int[N << 1];
+        
+        for (int i = 0; i < n; i++) {
+            segTree[N + i] = baskets[i];
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            segTree[i] = Math.max(segTree[2 * i], segTree[2 * i + 1]);
+        }
+
         int count = 0;
         
-        if (m == 0) {
-            return fruits.length;
-        }
-        
-        Arrays.fill(segTree, Integer.MIN_VALUE);
-        build(1, 0, m - 1);
-        
-        for (int i = 0; i < fruits.length; i++) {
-            int l = 0;
-            int r = m - 1;
-            int res = -1;
+        for (int i = 0; i < n; ++i) {
+            int x = fruits[i];
+            int index = 1;
+
+            if (segTree[index] < x) {
+                count++;
+                continue;
+            }
             
-            while (l <= r) {
-                int mid = (l + r) >> 1;
-                
-                if (query(1, 0, m - 1, 0, mid) >= fruits[i]) {
-                    res = mid;
-                    r = mid - 1;
+            while (index < N) {
+                if (segTree[index * 2] >= x) {
+                    index *= 2;
                 } else {
-                    l = mid + 1;
+                    index = index * 2 + 1;
                 }
             }
 
-            if (res != -1 && baskets[res] >= fruits[i]) {
-                update(1, 0, m - 1, res, Integer.MIN_VALUE);
-            } else {
-                count++;
+            segTree[index] = -1;
+            
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = Math.max(segTree[2 * index], segTree[2 * index + 1]);
             }
         }
         
-        return count;
+        return count;     
     }
 }
