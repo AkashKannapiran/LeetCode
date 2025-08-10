2fbd3625793e122e50b151d4672bf4208683ec11
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-08-10 09:03:28 +0530

    Time: 0 ms (100%), Space: 41.1 MB (36.84%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    private int[] digitFreq(int n) {
+        int[] freq = new int[10];
+        
+        while (n > 0) {
+            freq[n % 10]++;
+            n /= 10;
+        }
+
+        return freq;
+    }
+
+    private boolean equalFreq(int[] a, int[] b) {
+        
+        for (int i = 0; i < 10; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean reorderedPowerOf2(int N) {
+        
+        int[] target = digitFreq(N);
+
+        for (int i = 0; i <= 30; i++) {
+            int powerOfTwo = (int) Math.pow(2, i);
+
+            if (equalFreq(target, digitFreq(powerOfTwo))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
