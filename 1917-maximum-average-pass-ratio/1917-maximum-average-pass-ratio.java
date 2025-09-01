8c17387c83c59c8d30fe3f162f20601cc2b2d4ee
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-09-01 10:03:40 +0530

    Time: 287 ms (98.96%), Space: 84.4 MB (82.9%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,48 +1,42 @@
+
 class Solution {
-
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<double[]> maxHeap = new PriorityQueue<>((a, b) ->
-            Double.compare(b[0], a[0])
-        );
+        PriorityQueue<double[]> pq = new PriorityQueue<>(new Comparator<double[]>() {
+            public int compare(double[] a, double[] b) {
+                if (a[0] < b[0])
+                    return 1;
+                if (a[0] > b[0])
+                    return -1;
+                return 0;
+            }
+        });
 
-        for (int[] singleClass : classes) {
-            int passes = singleClass[0];
-            int totalStudents = singleClass[1];
-            double gain = calculateGain(passes, totalStudents);
-            maxHeap.offer(new double[] { gain, passes, totalStudents });
-        }
-
-        while (extraStudents-- > 0) {
-            double[] current = maxHeap.poll();
-            double currentGain = current[0];
-            int passes = (int) current[1];
-            int totalStudents = (int) current[2];
+        for (int i = 0; i < classes.length; i++) {
+            double pass = classes[i][0];
+            double total = classes[i][1];
+            double inc = (pass + 1.0) / (total + 1.0) - pass / total;
             
-            maxHeap.offer(
-                new double[] {
-                    calculateGain(passes + 1, totalStudents + 1),
-                    passes + 1,
-                    totalStudents + 1,
-                }
-            );
+            pq.offer(new double[] { inc, pass, total });
         }
 
-        double totalPassRatio = 0;
+        while (extraStudents > 0) {
+            double[] top = pq.poll();
+            double pass = top[1] + 1;
+            double total = top[2] + 1;
+            double inc = (pass + 1.0) / (total + 1.0) - pass / total;
+            
+            pq.offer(new double[] { inc, pass, total });
+            extraStudents--;
+        }
+
+        double sum = 0.0;
+        Object[] arr = pq.toArray();
         
-        while (!maxHeap.isEmpty()) {
-            double[] current = maxHeap.poll();
-            int passes = (int) current[1];
-            int totalStudents = (int) current[2];
-            totalPassRatio += (double) passes / totalStudents;
+        for (int i = 0; i < arr.length; i++) {
+            double[] c = (double[]) arr[i];
+            sum += c[1] / c[2];
         }
 
-        return totalPassRatio / classes.length;
-    }
-
-    private double calculateGain(int passes, int totalStudents) {
-        return (
-            (double) (passes + 1) / (totalStudents + 1) -
-            (double) passes / totalStudents
-        );
+        return sum / classes.length;
     }
 }
