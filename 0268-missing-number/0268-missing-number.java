0d05957f594f974b272bbf321fa29040a677b057
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-09-25 21:04:12 +0530

    Time: 2 ms (25.72%), Space: 45.6 MB (21.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+
+        return expectedSum - actualSum;
+    }
+}
