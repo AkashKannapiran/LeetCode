e572fc6eef6a14568422d139a278cc228cd32be4
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-09-28 17:47:00 +0530

    Time: 1 ms (100%), Space: 45.4 MB (27.16%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -1,13 +1,33 @@
 class Solution {
-    public int largestPerimeter(int[] A) {
-        Arrays.sort(A);
+    public int largestPerimeter(int[] nums) {
+        int n = nums.length;
         
-        for (int i = A.length - 3; i >= 0; --i) {
-            if (A[i] + A[i+1] > A[i+2]) {
-                return A[i] + A[i+1] + A[i+2];
+        selectAndSwapMax(nums, n - 1);
+        selectAndSwapMax(nums, n - 2);
+        
+        for (int i = n - 1; i >= 2; i--) {
+            selectAndSwapMax(nums, i - 2);
+            
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+
+    private void selectAndSwapMax(int[] nums, int idx) {
+        int maxValue = nums[0];
+        int maxIndex = 0;
+        
+        for (int i = 1; i <= idx; i++) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+                maxIndex = i;
             }
         }
         
-        return 0;
+        int temp = nums[idx];
+        nums[idx] = maxValue;
+        nums[maxIndex] = temp;
     }
 }
