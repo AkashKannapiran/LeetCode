7874cebb6eaf21c6e42f42d116ef59df4b66748a
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-09-30 09:14:14 +0530

    Time: 2 ms (100%), Space: 44.4 MB (78.77%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -1,22 +1,82 @@
+import java.util.*;
+
 class Solution {
+    private static final int[][] C5 = buildC5();
 
     public int triangularSum(int[] nums) {
-        List<Integer> current = Arrays.stream(nums)
-                                .boxed()
-                                .collect(Collectors.toList());
+        int n = nums.length;
+        int N = n - 1;
+        int ans = 0;
         
-        while (current.size() > 1) {
-            List<Integer> newNums = new ArrayList<>();
-            
-            for (int i = 0; i < current.size() - 1; ++i) {
-                newNums.add((current.get(i) + current.get(i + 1)) % 10);
-            }
+        for (int i = 0; i < n; i++) {
+            int c2 = combMod2(N, i);
+            int c5 = combMod5(N, i);
+            int c10 = crt10(c2, c5);
 
-            current = newNums;
+            ans = (ans + c10 * nums[i]) % 10;
         }
 
-        return current.get(0);
+        return ans;
 
     }
 
-}
+    private static int combMod2(int N, int i) {
+        return ((i & (N - i)) == 0) ? 1 : 0;
+    }
+    
+    private static int combMod5(int N, int i) {
+        int res = 1;
+        int n = N, k = i;
+        
+        while (n > 0 || k > 0) {
+            int nd = n % 5;
+            int kd = k % 5;
+            
+            if (kd > nd) {
+                return 0;
+            }
+
+            res = (res * C5[nd][kd]) % 5;
+            n /= 5;
+            k /= 5;
+        }
+        
+        return res;
+        
+    }
+
+    private static int crt10(int r2, int r5) {
+        int r = r5;
+        
+        if ((r & 1) != r2) {
+            r += 5;
+        }
+
+        return r;
+
+    }
+
+    private static int[][] buildC5() {
+        int[][] c = new int[5][5];
+        
+        for (int a = 0; a < 5; a++) {
+            c[a][0] = c[a][a] = 1;
+            
+            for (int b = 1; b < a; b++) {
+                c[a][b] = (c[a-1][b-1] + c[a-1][b]) % 5;
+            }
+
+        }
+
+        return c;
+
+    }
+    
+    static int triangularSumSlow(int[] nums) {
+                                                                                                                                                                                                                                                                                                                                                            int[] a = Arrays.copyOf(nums, nums.length);
+                                                                                                                                                                                                                                                                                                                                                                    for (int len = a.length; len > 1; len--) {
+                                                                                                                                                                                                                                                                                                                                                                                for (int j = 0; j < len - 1; j++) a[j] = (a[j] + a[j+1]) % 10;
+                                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                                return a[0];
+                                                                                                                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                                                                                                                    }
