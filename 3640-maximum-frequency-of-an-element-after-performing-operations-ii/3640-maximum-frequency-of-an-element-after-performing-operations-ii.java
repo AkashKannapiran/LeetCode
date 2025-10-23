4a0d719d1c8c179cc6277b14895dc5fa726a7388
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-10-23 13:50:41 +0530

    Time: 31 ms (93.2%), Space: 56.4 MB (100%) - LeetHub

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,86 +1,44 @@
 class Solution {
-    
-    public int maxFrequency(int[] nums, int k, int numOperations) {
+    public int maxFrequency(int[] nums, int k, int ops) {
+        int res = 0;
         Arrays.sort(nums);
-        int ans = 0;
-        Map<Integer, Integer> numCount = new HashMap<>();
-        Set<Integer> modes = new TreeSet<>();
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+        int i = 0;
         
-        Consumer<Integer> addMode = value -> {
-            modes.add(value);
-
-            if (value - k >= nums[0]) {
-                modes.add(value - k);
+        while (i < n) {
+            int val = nums[i];
+            int same = 0;
+            
+            while (i < n && nums[i] == val) {
+                same++;
+                i++;
             }
-
-            if (value + k <= nums[nums.length - 1]) {
-                modes.add(value + k);
+            
+            while (right < n && nums[right] <= val + k) {
+                right++;
             }
-        };
-
-        int lastNumIndex = 0;
-
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != nums[lastNumIndex]) {
-                numCount.put(nums[lastNumIndex], i - lastNumIndex);
-                ans = Math.max(ans, i - lastNumIndex);
-                addMode.accept(nums[lastNumIndex]);
-                lastNumIndex = i;
+            
+            while (left < n && nums[left] < val - k) {
+                left++;
             }
+            
+            res = Math.max(res, Math.min(same + ops, right - left));
         }
 
-        numCount.put(nums[lastNumIndex], nums.length - lastNumIndex);
-        ans = Math.max(ans, nums.length - lastNumIndex);
-        addMode.accept(nums[lastNumIndex]);
+        left = 0;
+        right = 0;
         
-        IntUnaryOperator leftBound = value -> {
-            int left = 0;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int mid = (left + right) / 2;
-                
-                if (nums[mid] < value) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
-
-            return left;
-        };
-
-        IntUnaryOperator rightBound = value -> {
-            int left = 0;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int mid = (left + right + 1) / 2;
-                
-                if (nums[mid] > value) {
-                    right = mid - 1;
-                } else {
-                    left = mid;
-                }
-            }
-
-            return left;
-        };
-
-        for (int mode : modes) {
-            int l = leftBound.applyAsInt(mode - k);
-            int r = rightBound.applyAsInt(mode + k);
-            int tempAns;
-            
-            if (numCount.containsKey(mode)) {
-                tempAns = Math.min(r - l + 1, numCount.get(mode) + numOperations);
-            } else {
-                tempAns = Math.min(r - l + 1, numOperations);
+        while (right < n) {
+            while (right < n && (long) nums[left] + k + k >= nums[right]) {
+                right++;
             }
             
-            ans = Math.max(ans, tempAns);
+            res = Math.max(res, Math.min(right - left, ops));
+            left++;
         }
         
-        return ans;
+        return res;
     }
 }
