4113b1660a3575a3fa50204163c98920df5dfe17
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-12-06 17:40:19 +0530

    Time: 38 ms (95.06%), Space: 69.5 MB (58.02%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -1,47 +1,45 @@
 class Solution {
 
-    public int countPartitions(int[] nums, int k) {
-
-        int n = nums.length;
-        long mod = (long) 1e9 + 7;
-        long[] dp = new long[n + 1];
-        long[] prefix = new long[n + 1];
-
-        Deque<Integer> minQ = new LinkedList<>();
-        Deque<Integer> maxQ = new LinkedList<>();
-
+    public int countPartitions(int[] A, int k) {
+        int n = A.length;
+        int mod = 1_000_000_007;
+        int acc = 1;
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        prefix[0] = 1;
 
-        for (int i = 0, j = 0; i < n; i++) {
-            while (!maxQ.isEmpty() && nums[maxQ.peekLast()] <= nums[i]) {
-                maxQ.pollLast();
+        Deque<Integer> minq = new ArrayDeque<>();
+        Deque<Integer> maxq = new ArrayDeque<>();
+
+        for (int i = 0, j = 0; j < n; ++j) {
+            while (!maxq.isEmpty() && A[j] > A[maxq.peekLast()]) {
+                maxq.pollLast();
             }
 
-            maxQ.offerLast(i);
+            maxq.addLast(j);
 
-            while (!minQ.isEmpty() && nums[minQ.peekLast()] >= nums[i]) {
-                minQ.pollLast();
+            while (!minq.isEmpty() && A[j] < A[minq.peekLast()]) {
+                minq.pollLast();
             }
 
-            minQ.offerLast(i);
+            minq.addLast(j);
 
-            while (!maxQ.isEmpty() && !minQ.isEmpty() && nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > k) {
-                if (maxQ.peekFirst() == j) {
-                    maxQ.pollFirst();
+            while (A[maxq.peekFirst()] - A[minq.peekFirst()] > k) {
+                acc = (acc - dp[i++] + mod) % mod;
+
+                if (minq.peekFirst() < i) {
+                    minq.pollFirst();
                 }
 
-                if (minQ.peekFirst() == j) {
-                    minQ.pollFirst();
+                if (maxq.peekFirst() < i) {
+                    maxq.pollFirst();
                 }
-
-                j++;
             }
 
-            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
-            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+            dp[j + 1] = acc;
+            acc = (acc + dp[j + 1]) % mod;
         }
 
-        return (int) dp[n];
+        return dp[n];
+
     }
 }
