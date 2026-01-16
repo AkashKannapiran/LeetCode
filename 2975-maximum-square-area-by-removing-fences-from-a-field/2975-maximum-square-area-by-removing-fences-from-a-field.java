84da61f4b6068772cb29e41c715d1789d2c2c7da
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-01-16 13:18:18 +0530

    Time: 0 ms (100%), Space: 276.8 MB (9.52%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.java
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.java
@@ -1,41 +1,98 @@
 class Solution {
-
-    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
-        Set<Integer> hEdges = getEdges(hFences, m);
-        Set<Integer> vEdges = getEdges(vFences, n);
-        long res = 0;
-        
-        for (int e : hEdges) {
-            if (vEdges.contains(e)) {
-                res = Math.max(res, e);
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
             }
-        }
-
-        if (res == 0) {
-            return -1;
-        } else {
-            return (int) ((res * res) % 1000000007);
-        }
+        }));
     }
 
-    private Set<Integer> getEdges(int[] fences, int border) {
-        Set<Integer> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        long ans = 0;
+        Set<Integer> hSet = new HashSet<>();
+        hSet.add(m - 1);
 
-        for (int fence : fences) {
-            list.add(fence);
+        for (int i = 0; i < hFences.length; i++) {
+            hSet.add(hFences[i] - 1);
+            hSet.add(m - hFences[i]);
         }
 
-        list.add(1);
-        list.add(border);
-        Collections.sort(list);
-
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                set.add(list.get(j) - list.get(i));
+        for (int i = 0; i < hFences.length; i++) {
+            for (int j = 0; j < hFences.length; j++) {
+                if (i != j) {
+                    hSet.add(hFences[i] - hFences[j]);
+                }
             }
         }
 
-        return set;
+        if (hSet.contains(n - 1)) {
+            ans = Math.max(ans, n - 1);
+        }
+
+        for (int i = 0; i < vFences.length; i++) {
+            if (hSet.contains(vFences[i] - 1)) {
+                ans = Math.max(ans, vFences[i] - 1);
+            }
+
+            if (hSet.contains(n - vFences[i])) {
+                ans = Math.max(ans, n - vFences[i]);
+            }
+        }
+
+        for (int i = 0; i < vFences.length; i++) {
+            for (int j = 0; j < vFences.length; j++) {
+                if (i != j && hSet.contains(Math.abs(vFences[i] - vFences[j]))) {
+                    ans = Math.max(ans, Math.abs(vFences[i] - vFences[j]));
+                }
+            }
+        }
+
+        return ans == 0 ? -1 : (int) ((ans * ans) % 1000000007L);
+    }
+
+    private int getMaxSqArea(int largerSideLen, int smallerSideLen, int[] largerSideFences, int[] smallerSideFences) {
+        Arrays.sort(largerSideFences);
+        Arrays.sort(smallerSideFences);
+
+        Set<Integer> largerSideFencesSet = Arrays.stream(largerSideFences)
+                .boxed()
+                .collect(Collectors.toCollection(HashSet::new));
+
+        largerSideFencesSet.add(1);
+        largerSideFencesSet.add(largerSideLen);
+
+        Set<Integer> smallerSideFencesSet = Arrays.stream(smallerSideFences)
+                .boxed()
+                .collect(Collectors.toCollection(HashSet::new));
+
+        smallerSideFencesSet.add(1);
+        smallerSideFencesSet.add(smallerSideLen);
+
+        for (int maxPossEdge = smallerSideLen - 1; maxPossEdge > 0; maxPossEdge--) {
+            for (int currLargerSideFencesIndex = largerSideFences.length
+                    - 1; currLargerSideFencesIndex >= 0; currLargerSideFencesIndex--) {
+                if (largerSideFencesSet.contains(largerSideFences[currLargerSideFencesIndex] - maxPossEdge)
+                        || largerSideFencesSet.contains(largerSideLen - maxPossEdge)) {
+                    for (int currSmallerSideFencesIndex = smallerSideFences.length
+                            - 1; currSmallerSideFencesIndex >= 0; currSmallerSideFencesIndex--) {
+                        if (smallerSideFencesSet.contains(smallerSideFences[currSmallerSideFencesIndex] - maxPossEdge)
+                                || smallerSideFencesSet.contains(smallerSideLen - maxPossEdge)) {
+                            return (int) (((long) maxPossEdge * (long) maxPossEdge) % 1000000007);
+                        }
+
+                        if (smallerSideFences[currSmallerSideFencesIndex] - maxPossEdge < 0) {
+                            break;
+                        }
+                    }
+                }
+
+                if (largerSideFences[currLargerSideFencesIndex] - maxPossEdge < 0) {
+                    break;
+                }
+            }
+        }
+
+        return -1;
     }
 }
