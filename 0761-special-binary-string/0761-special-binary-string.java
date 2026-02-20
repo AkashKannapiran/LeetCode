33d300ef86d95e1cb46845e0622dce4d4baac0e2
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-02-20 13:39:35 +0530

    Time: 2 ms (100%), Space: 43.1 MB (89.57%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.java
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -1,22 +1,40 @@
 class Solution {
-    public String makeLargestSpecial(String s) {
-        int count = 0;
-        int i = 0;
-        List<String> res = new ArrayList<>();
+    public String makeLargestSpecial(String S) {
+        if (S == null || S.length() == 0 || S.length() == 2)
+            return S;
+        
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> b.compareTo(a));
+        int acc = 1, prev = 0;
+        
+        for (int i = 1; i <= S.length(); i++) {
+            if (acc == 0) {
+                if (!(prev == 0 && i == S.length()))
+                    pq.add(makeLargestSpecial(S.substring(prev, i)));
+                prev = i;
+            }
 
-        for (int j = 0; j < s.length(); j++) {
-            if (s.charAt(j) == '1')
-                count++;
-            else
-                count--;
-
-            if (count == 0) {
-                res.add('1' + makeLargestSpecial(s.substring(i + 1, j)) + '0');
-                i = j + 1;
+            if (i == S.length())
+                break;
+            
+            if (S.charAt(i) == '1') {
+                acc++;
+            } else {
+                acc--;
             }
         }
 
-        Collections.sort(res, Collections.reverseOrder());
-        return String.join("", res);
+        StringBuilder ans = new StringBuilder();
+        
+        while (!pq.isEmpty()) {
+            ans.append(pq.poll());
+        }
+        
+        if (ans.length() == 0) {
+            ans.append('1');
+            ans.append(makeLargestSpecial(S.substring(1, S.length() - 1)));
+            ans.append('0');
+        }
+        
+        return ans.toString();
     }
 }
