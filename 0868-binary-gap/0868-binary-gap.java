a3dad06ed3fc22fd35cbb4ea5cce7f6d279fd8cf
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-02-22 11:24:12 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.06%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -1,16 +1,23 @@
 class Solution {
-    public int binaryGap(int N) {
-        int last = -1;
-        int ans = 0;
-        
-        for (int i = 0; i < 32; ++i) {
-            if (((N >> i) & 1) > 0) {
-                if (last >= 0)
-                    ans = Math.max(ans, i - last);
-                last = i;
+    public int binaryGap(int n) {
+        int m = 0;
+        int l = -1;
+        int p = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (l != -1) {
+                    int d = p - l;
+                    m = Math.max(m, d);
+                }
+
+                l = p;
             }
+
+            n = n >> 1;
+            p++;
         }
 
-        return ans;
+        return m;
     }
 }
