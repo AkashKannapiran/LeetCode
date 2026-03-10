93738b17ef6dcacec410472fae14302a157e9952
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-03-10 07:41:21 +0530

    Time: 38 ms (100%), Space: 53.6 MB (90.8%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.java
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.java
@@ -1,45 +1,73 @@
 class Solution {
 
+    private static final int MOD = 1_000_000_007;
+
+    private int add(int a, int b) {
+        return (a + b) % MOD;
+    }
+
+    private int subtract(int a, int b) {
+        return (a - b + MOD) % MOD;
+    }
+
+    private int multiply(int a, int b) {
+        return (int) ((long) a * b % MOD);
+    }
+
     public int numberOfStableArrays(int zero, int one, int limit) {
-        final int MOD = 1000000007;
-        int[][][] dp = new int[zero + 1][one + 1][2];
 
-        for (int i = 0; i <= zero; i++) {
-            for (int j = 0; j <= one; j++) {
-                for (int lastBit = 0; lastBit <= 1; lastBit++) {
-                    if (i == 0) {
-                        if (lastBit == 0 || j > limit) {
-                            dp[i][j][lastBit] = 0;
-                        } else {
-                            dp[i][j][lastBit] = 1;
-                        }
-                    } else if (j == 0) {
-                        if (lastBit == 1 || i > limit) {
-                            dp[i][j][lastBit] = 0;
-                        } else {
-                            dp[i][j][lastBit] = 1;
-                        }
-                    } else if (lastBit == 0) {
-                        dp[i][j][lastBit] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                        if (i > limit) {
-                            dp[i][j][lastBit] -= dp[i - limit - 1][j][1];
-                        }
-                    } else {
-                        dp[i][j][lastBit] = dp[i][j - 1][0] + dp[i][j - 1][1];
-                        if (j > limit) {
-                            dp[i][j][lastBit] -= dp[i][j - limit - 1][0];
-                        }
-                    }
+        int m = Math.max(zero, one);
 
-                    dp[i][j][lastBit] %= MOD;
+        int[][] sumLimit = new int[m + 1][m + 1];
 
-                    if (dp[i][j][lastBit] < 0) {
-                        dp[i][j][lastBit] += MOD;
-                    }
+        int[] prefix = new int[m + 1];
+        int[] prev = sumLimit[0];
+
+        prev[0] = 1;
+
+        for (int count = 1; count <= m; count++) {
+
+            int maxSum = Math.min(count * limit, m);
+            int running = 0;
+
+            for (int i = 0; i <= maxSum; i++) {
+                prefix[i] = running;
+                running = add(running, prev[i]);
+            }
+
+            int[] curr = sumLimit[count];
+
+            for (int i = count; i <= maxSum; i++) {
+                if (i < limit) {
+                    curr[i] = prefix[i];
+                } else {
+                    curr[i] = subtract(prefix[i], prefix[i - limit]);
                 }
             }
+
+            prev = curr;
         }
 
-        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+        int result = (m <= limit) ? 2 : 0;
+
+        int c0 = sumLimit[1][zero];
+        int c1 = sumLimit[1][one];
+
+        for (int i = 2; i <= m; i++) {
+
+            int prevC0 = c0;
+            int prevC1 = c1;
+
+            c0 = sumLimit[i][zero];
+            c1 = sumLimit[i][one];
+
+            result = add(result, multiply(c0, prevC1));
+            result = add(result, multiply(c1, prevC0));
+
+            int t = multiply(c0, c1);
+            result = add(result, add(t, t));
+        }
+
+        return result;
     }
 }
