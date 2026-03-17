8ee9656c29742f536003e941e34b112682142213
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-03-17 10:15:50 +0530

    Time: 12 ms (100%), Space: 113.9 MB (66.96%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -2,36 +2,29 @@ class Solution {
     public int largestSubmatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        List<Pair<Integer, Integer>> prevHeights = new ArrayList();
-        int ans = 0;
 
-        for (int row = 0; row < m; row++) {
-            List<Pair<Integer, Integer>> heights = new ArrayList();
-            boolean[] seen = new boolean[n];
-
-            for (Pair<Integer, Integer> pair : prevHeights) {
-                int height = pair.getKey();
-                int col = pair.getValue();
-
-                if (matrix[row][col] == 1) {
-                    heights.add(new Pair(height + 1, col));
-                    seen[col] = true;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    continue;
                 }
-            }
 
-            for (int col = 0; col < n; col++) {
-                if (seen[col] == false && matrix[row][col] == 1) {
-                    heights.add(new Pair(1, col));
-                }
+                matrix[i][j] += matrix[i - 1][j];
             }
-
-            for (int i = 0; i < heights.size(); i++) {
-                ans = Math.max(ans, heights.get(i).getKey() * (i + 1));
-            }
-
-            prevHeights = heights;
         }
 
-        return ans;
+        int maxArea = 0;
+
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(matrix[i]);
+
+            for (int j = 0; j < n; j++) {
+                int width = n - j;
+                maxArea = Math.max(maxArea, width * matrix[i][j]);
+
+            }
+        }
+
+        return maxArea;
     }
 }
